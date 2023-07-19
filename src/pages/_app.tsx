b5c2f46c3dcb64688baf4296df589605ed617e81
Muhammad Rehan <muhd.rehan@hotmail.com>
2023-07-19 07:58:13 +0500

    Updated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Rootlayout from "@/components/header/Rootlayout";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Provider } from "react-redux";
+import { store } from "@/store/store";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <div className="font-bodyfont bg-gray-300">
+        <Rootlayout>
+          <Component {...pageProps} />
+        </Rootlayout>
+      </div>
+    </Provider>
+  );
 }
